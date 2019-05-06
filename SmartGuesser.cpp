79877a66c1dc35b38fb9fc7da484652621060f3e
Chen Ostrovski <chenostrovski@gmail.com>
2019-05-06 18:59:36 +0300

    Add files via upload

--- SmartGuesser.cpp
+++ SmartGuesser.cpp
@@ -2,50 +2,73 @@
 #include <string>
 #include "SmartGuesser.hpp"
 #include "Guesser.hpp"
+#include "calculate.hpp"
 #include <vector>
 using std::string;
+using namespace bullpgia;
 
 //http://www.cplusplus.com/forum/beginner/148637/
 string bullpgia::SmartGuesser::guess() {
 	int j=0;
+	string res = "";
 	for(int i=0; i<length; i++){
 		nums[i]=j;
-		if(){
+		if(i==0){
+			if(bullpgia::calculateBullAndPgia(nums, key)=="1,0"){
+			res = res + std::to_string(j);
 			j=0;
-		}
-		else{
-			if(j>=9){
-				j=0;
+			}
+			else{
+				j++;
 			}
 		}
+		else if(i==1){
+			if(bullpgia::calculateBullAndPgia(nums, key)=="2,0"){
+			res = res + std::to_string(j);
+			j=0;
+				}
+			else{
+				j++;
+			}
+		}
+		else if(i==2){
+			if(bullpgia::calculateBullAndPgia(nums, key)=="3,0"){
+			res = res + std::to_string(j);
+			j=0;
+				}
+			else{
+				j++;
+			}
+		}
+		else if(i==3){
+			if(bullpgia::calculateBullAndPgia(nums, key)=="4,0"){
+			res = res + std::to_string(j);
+			j=0;
+			}
+			else{
+				j++;
+			}
+		}
+		if(j>9){
+			j=0;
+		}
 	}
-	return mguess;
+	return res;
 }
 
 void bullpgia::SmartGuesser::startNewGame(uint length) {
 	this->length = length;
-	nums.erase (nums.begin(),nums.end()+0);
-	key.erase (key.begin(),key.end()+0);
+	nums="";
+	key="";
 	track=0;
-}
-void bullpgia::SmartGuesser::init_nums(vector<char>& nums){
-
-}
-void bullpgia::SmartGuesser::get_key(vector<char>& key){
-	for (short int i{ 0 }; i < length; i++) {
-		char c;
-
-	}
-
 }
 
 void bullpgia::SmartGuesser::learn(string reply) {
 	std::size_t found = reply.find(",");
 	int numberBull = stoi(reply.substr(0, found));
 	int numberPgia = stoi(reply.substr(found + 1, reply.length()));
-	    
-    if (numberBull == length) {
-        break;
-    }
-	get_key(key);
+
+	if(numberBull==length){
+		return;
+	}
 }
--- SmartGuesser.hpp
+++ SmartGuesser.hpp
@@ -9,8 +9,8 @@ using std::string, std::istream, std::iostream;
 namespace bullpgia{
     class SmartGuesser : public bullpgia::Guesser{
         string mguess;
-        vector<char> nums; //vector for user's guesses
-        vector<char> key; //the digit code
+        string nums; //vector for user's guesses
+        string key; //the digit code
         int track=0; //this variable helps handling various trackings in the while loop and increases by 1 each time that the user enters a number
 
         public:
@@ -18,8 +18,6 @@ namespace bullpgia{
         {
             this->mguess="";
         }
-        void init_nums(vector<char>& nums);
-        void get_key(vector<char>& key);
         string guess() override;
         void startNewGame(uint length) override;
         void learn(string reply) override;
